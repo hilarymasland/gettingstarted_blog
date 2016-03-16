1c862e42f3e853cf6c152327cb0770c0d7dd436c
Hilary Masland <hilarymasland@gmail.com>
2016-03-16 18:04:51 -0400

    3rd commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,10 @@
-  class ArticlesController < ApplicationController
-    def index
-      @articles = Article.all
-    end
+class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+  def index
+    @articles = Article.all
+  end
     def show
         @article = Article.find(params[:id])
       end
@@ -28,6 +31,12 @@
   else
     render 'edit'
   end
+end
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+
+  redirect_to articles_path
 end
       private
         def article_params
